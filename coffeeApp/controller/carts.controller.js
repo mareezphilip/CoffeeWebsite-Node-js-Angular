8cb960751977fdc0fcee9527a3568a0b18ebce5e
mareez philip <mareezphilip7@gmail.com>
2023-09-23 20:29:07 +0300

    cart model

--- coffeeApp/controller/carts.controller.js
+++ coffeeApp/controller/carts.controller.js
@@ -14,10 +14,21 @@ class cart {
          }
          else{   
              const cart = await cartModel.find({userId: req.user._id})
-             console.log(cart)
-             cart[0].products.push((req.body.products[0]))
+
+             const productfound = cart[0].products.findIndex(p=> p.productid == req.body.products[0].productid )
+             if(productfound){
+               cart[0].products[productfound].quantity +=req.body.products[0].quantity
+
+             }
+             else{
+              console.log(cart)
+              cart[0].products.push((req.body.products[0]))
+              
+             }
              await cart[0].save()
              resGenerator(res, 200, true, cart[0], "added to cart")
+
+            
          }
          
         }
@@ -25,7 +36,87 @@ class cart {
          resGenerator(res, 500, false, e.message, "error in insert")
         }
  
-     }
+    }
 
-}
+    static showAllcarts = async(req,res) =>{
+      try {
+          const carts = await cartModel.find()
+          resGenerator(res, 200, true, carts, "data showed")
+      }
+      catch (e) {
+          resGenerator(res, 500, false, e.message, "error in show data")
+      }
+    }
+
+  static singleCart = async (req, res) => {
+    try {
+        const userCart = await cartModel.find( {userId: req.user._id})  // null
+        if (!userCart[0])
+            resGenerator(res, 200, true, userCart[0], "empty cart")
+        resGenerator(res, 200, true, userCart[0], "data showed")
+    }
+    catch (e) {
+        resGenerator(res, 500, false, e, "error in show data")
+    }
+  }
+  static incProductQuantity = async(req,res)=>{
+    try{
+      const cart = await cartModel.find({userId:req.user._id})
+     const productindex = cart[0].products.findIndex(p=> p.productid == req.params.pId )
+     if(productindex) {
+        cart[0].products[productindex].quantity += 1
+        await cart[0].save()
+        resGenerator(res, 200, true, cart[0], "quantity updated")
+     }
+    }
+     catch(e){
+      resGenerator(res, 500, false, e.message, "error in edit")
+     }
+  }
+
+  static decProductQuantity = async(req,res)=>{
+    try{
+      const cart = await cartModel.find({userId:req.user._id})
+     const productindex = cart[0].products.findIndex(p=> p.productid == req.params.pId )
+     if(productindex) {
+        cart[0].products[productindex].quantity -= 1
+        await cart[0].save()
+        resGenerator(res, 200, true, cart[0], "quantity updated")
+     }
+    }
+     catch(e){
+      resGenerator(res, 500, false, e.message, "error in edit")
+     }
+  }
+
+  static deleteProductCart = async(req,res)=>{
+    try{
+      const cart = await cartModel.find({userId:req.user._id})
+     const productindex = cart[0].products.findIndex(p=> p.productid == req.params.pId )
+     if(productindex) {
+        cart[0].products.splice(productindex,1)
+        await cart[0].save()
+        resGenerator(res, 200, true, cart[0], "product deleted")
+     }
+    }
+     catch(e){
+      resGenerator(res, 500, false, e.message, "error in delete product")
+     }
+  }
+  static deleteAllProductCart = async(req,res)=>{
+    try{
+      const cart = await cartModel.find({userId:req.user._id})
+      if(cart[0]){
+        cart[0].products = []
+        await cart[0].save()
+        resGenerator(res, 200, true, cart[0], "products deleted")
+      }
+    }
+    catch(e){
+      resGenerator(res, 500, false, e.message, "error in delete product")
+    }
+    }
+
+  }
+
 module.exports=cart
--- routes/cart.route.js
+++ routes/cart.route.js
@@ -5,5 +5,11 @@ const router = require("express").Router()
 
 
 router.post("/addtocart", authUser, cartcontroller.addcart )
+router.get("/allCarts" ,authUser , authAdmin , cartcontroller.showAllcarts )
+router.get("/userCart" ,authUser , cartcontroller.singleCart )
+router.patch("/incProductQuantity/:pId",authUser ,cartcontroller.incProductQuantity)
+router.patch("/decProductQuantity/:pId",authUser ,cartcontroller.decProductQuantity)
+router.delete("/deleteProductCart/:pId" , authUser  , cartcontroller.deleteProductCart)
+router.delete("/deleteAllProductCart" , authUser  , cartcontroller.deleteAllProductCart)
 
 module.exports = router
